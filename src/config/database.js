a2ddf5b27a1e38fa29950c25e5a967c6dd3b4959
Jose Campos <joseluiscampostorres10@gmail.com>
2026-05-20 23:25:20 -0500

    envio nuevo registro2588

--- src/config/database.js
+++ src/config/database.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: "mysql",
-    port: process.env.DB_PORT,
+    // port: process.env.DB_PORT,
     logging: false,
 
     // 1. Le dice a Sequelize cómo interpretar los objetos de fecha internamente
